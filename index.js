439153e0b0d733cf0d6fcc609291f22d33b54f0c
cheshirecatalyst <dekathexis@gmail.com>
2015-03-03 03:41:30 +0700

    more refactoring of new 'class' modules

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ var senecaDynamoDB = require('seneca-dynamo-store');
 
 // Load project libraries and configuration
 var config = require('./config.js');
-var callback = require('./lib/callback.js');
+var Callback = require('./lib/callback.js');
 var exportData = require('./lib/export-data.js');
 var mysqlTools = require('./lib/mysql-tools.js');
-var synchronize = require('./lib/synchronize.js');
-var healthyCheck = require('./lib/healthy-check.js');
+var Synchronize = require('./lib/synchronize.js');
+var HealthyCheck = require('./lib/healthy-check.js');
 var errorCollector = require('./lib/error-collector.js');
 
 var mysqlConnection;
 
 seneca.use(senecaDynamoDB, config.database.dynamodb);
-
 
 // Create a server with a host and port
 var server = new hapiModule.Server();
@@ -33,28 +32,40 @@ errorCollector.setRequestModule(requestModule);
 errorCollector.setHostname(server.info.uri);
 errorCollector.setTargetUrl(config.errorTracker.url);
 
+
+
 // Set up export data
-exportData.setRequestModule(requestModule);
-exportData.setHostname(server.info.uri);
-exportData.setTargetUrl(config.exportData.url);
+// files that need it , now require it
+// exportData.setRequestModule(requestModule);
+// exportData.setHostname(server.info.uri);
+// exportData.setTargetUrl(config.exportData.url);
 
 // Connect to MySQL
 mysqlTools.setErrorCollector(errorCollector);
 mysqlConnection = mysqlTools.connectToMysql(mysqlModule, config);
 
 // Set handler params
-callback.setErrorCollector(errorCollector);
-callback.setSenecaReference(seneca);
-callback.setValidator(validatorModule);
+handlerParams = {
+    errorCollector: errorCollector,
+    seneca: seneca,
+    validatorModule: validatorModule
+};
+callback = new Callback(handlerParams);
+// callback.init(handlerParams);
 
 // Configure healthy check
-healthyCheck.setSenecaReference(seneca);
+healthyParams = {
+    seneca: seneca
+};
+healthyCheck = new HealthyCheck(healthyParams);
 
 // Set handler params
-synchronize.setErrorCollector(errorCollector);
-synchronize.setMysqlConnection(mysqlConnection);
-synchronize.setExportData(exportData);
-synchronize.setLimit(config.synchronize.maxItems);
+synchronizeParams = {
+    errorCollector: errorCollector,
+    seneca: seneca,
+    limit: config.synchronize.maxItems
+};
+synchronize = new Synchronize(synchronizeParams);
 
 // Add the routes and their handlers
 server.route({
