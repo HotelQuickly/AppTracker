1b5fb28e35f6b14b88d182933459e34da5fd3f95
cheshirecatalyst <dekathexis@gmail.com>
2015-03-04 11:53:44 +0700

    more pruning

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ var senecaDynamoDB = require('seneca-dynamo-store');
 // Load project libraries and configuration
 var config = require('./config.js');
 var Callback = require('./lib/callback.js');
-var exportData = require('./lib/export-data.js');
 var Synchronize = require('./lib/synchronize.js');
 var HealthyCheck = require('./lib/healthy-check.js');
-var errorCollector = require('./lib/error-collector.js');
 
 seneca.use(senecaDynamoDB, config.database.dynamodb);
 
@@ -23,58 +21,30 @@ seneca.use(senecaDynamoDB, config.database.dynamodb);
 var server = new hapiModule.Server();
 server.connection({ host: '0.0.0.0', port: process.env.PORT || 3000 });
 
-// Set up error tracker
-errorCollector.setRequestModule(requestModule);
-errorCollector.setHostname(server.info.uri);
-errorCollector.setTargetUrl(config.errorTracker.url);
-
 // Set handler params
 handlerParams = {
-    errorCollector: errorCollector,
-    seneca: seneca,
-    validatorModule: validatorModule
+	seneca: seneca,
+	validatorModule: validatorModule
 };
 callback = new Callback(handlerParams);
 
 // Set healthy check params
 healthyParams = {
-    seneca: seneca
+	seneca: seneca
 };
 healthyCheck = new HealthyCheck(healthyParams);
 
 // Set handler params
 synchronizeParams = {
-    errorCollector: errorCollector,
-    seneca: seneca,
-    limit: config.synchronize.maxItems
+	seneca: seneca,
+	limit: config.synchronize.maxItems
 };
 synchronize = new Synchronize(synchronizeParams);
 
-// Add the routes and their handlers
-server.route({
-    method: 'POST',
-    path: '/callback',
-    handler: callback.handlerSaveCallback
-});
-server.route({
-    method: 'POST',
-    path: '/', /* we need this route for legacy reasons, it's the same as POST /callback */
-    handler: callback.handlerSaveCallback
-});
-server.route({
-    method: 'GET',
-    path: '/healthy-check',
-    handler: healthyCheck.handlerHealthyCheck
-});
-server.route({
-	method: 'GET',
-	path: '/sync',
-	handler: synchronize.handlerSync
-});
 
 // Start server
 seneca.ready( function(){
-    server.start(function () {
-        console.log('Server started at: ' + server.info.uri);
-    });
+	server.start(function () {
+		console.log('Server started at: ' + server.info.uri);
+	});
 });
